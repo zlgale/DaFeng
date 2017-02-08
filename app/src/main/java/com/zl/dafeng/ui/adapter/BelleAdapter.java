927447f2e65dd54b1322dc9ae8fdce638e1ee8dd
yongbu <598787663@qq.com>
2017-02-08 17:27:08 +0800

    BaseRecyclerViewAdapterHelper 的使用

--- app/src/main/java/com/zl/dafeng/ui/adapter/BelleAdapter.java
+++ app/src/main/java/com/zl/dafeng/ui/adapter/BelleAdapter.java
@@ -2,17 +2,14 @@ package com.zl.dafeng.ui.adapter;
 
 import android.content.Context;
 import android.net.Uri;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
 import com.facebook.drawee.view.SimpleDraweeView;
-import com.yanzhenjie.recyclerview.swipe.SwipeMenuAdapter;
 import com.zl.dafeng.R;
 import com.zl.dafeng.bo.model.BelleModel;
-import com.zl.dafeng.service.interfaces.OnItemClickListener;
 
 import java.util.List;
 
@@ -23,80 +20,26 @@ import java.util.List;
  * @email 598787663@qq.com
  */
 
-public class BelleAdapter extends SwipeMenuAdapter<BelleAdapter.DefaultViewHolder> {
+public class BelleAdapter extends BaseQuickAdapter<BelleModel.ShowapiResBodyBean.NewslistBean,BaseViewHolder> {
 
     protected Context mContext;
-    protected int mLayoutId;
-    protected List<BelleModel.ShowapiResBodyBean.NewslistBean> mDatas;
-    private OnItemClickListener mOnItemClickListener;
 
-    public BelleAdapter(Context mContext, List<BelleModel.ShowapiResBodyBean.NewslistBean> mDatas) {
+    public BelleAdapter(Context mContext, List<BelleModel.ShowapiResBodyBean.NewslistBean> data) {
+        super(R.layout.item_belle, data);
         this.mContext = mContext;
-        this.mDatas = mDatas;
     }
-
     @Override
-    public View onCreateContentView(ViewGroup parent, int viewType) {
-        return LayoutInflater.from(parent.getContext()).inflate(R.layout.item_belle, parent, false);
-    }
-
-    @Override
-    public DefaultViewHolder onCompatCreateViewHolder(View realContentView, int viewType) {
-        return new DefaultViewHolder(realContentView);
-    }
-
-    @Override
-    public void onBindViewHolder(DefaultViewHolder holder, int position) {
-
-        holder.setData(mDatas.get(position));
-        holder.setOnItemClickListener(mOnItemClickListener);
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return this.mDatas != null ? mDatas.size() : 0;
-    }
-
-    //    class BelleViewHolder extends RecyclerView.ViewHolder {
-//        SimpleDraweeView belleImg;
-//
-//        public BelleViewHolder(View itemView) {
-//            super(itemView);
-//            belleImg = (SimpleDraweeView) itemView.findViewById(R.id.belle_img);
-//        }
-//    }
-    class DefaultViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        SimpleDraweeView belleImg;
-        OnItemClickListener mOnItemClickListener;
-
-        public DefaultViewHolder(View itemView) {
-            super(itemView);
-            itemView.setOnClickListener(this);
-            belleImg = (SimpleDraweeView) itemView.findViewById(R.id.belle_img);
-        }
-
-        public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
-            this.mOnItemClickListener = onItemClickListener;
-        }
-
-        public void setData(final BelleModel.ShowapiResBodyBean.NewslistBean myChooseType) {
-            belleImg.setImageURI(Uri.parse(myChooseType.getPicUrl()));
-            belleImg.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(mContext, "点击了", Toast.LENGTH_LONG).show();
+    protected void convert(BaseViewHolder baseViewHolder, BelleModel.ShowapiResBodyBean.NewslistBean newslistBean) {
+        SimpleDraweeView belleImg = baseViewHolder.getView(R.id.belle_img);
+        belleImg.setImageURI(Uri.parse(newslistBean.getPicUrl()));
+        belleImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "点击了", Toast.LENGTH_LONG).show();
 //                    Intent intent = new Intent(mContext, MaterialPagerActivity.class);
 //                    mContext.startActivity(intent);
-                }
-            });
-        }
-
-        @Override
-        public void onClick(View v) {
-            if (mOnItemClickListener != null) {
-                mOnItemClickListener.onItemClick(getAdapterPosition());
             }
-        }
+        });
     }
+
 }
--- app/src/main/java/com/zl/dafeng/ui/fragment/GirlFragment.java
+++ app/src/main/java/com/zl/dafeng/ui/fragment/GirlFragment.java
@@ -1,9 +1,14 @@
 package com.zl.dafeng.ui.fragment;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,8 +17,11 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.animation.BaseAnimation;
 import com.google.gson.Gson;
-import com.yanzhenjie.recyclerview.swipe.SwipeMenuRecyclerView;
+import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
+import com.yqritc.recyclerviewflexibledivider.VerticalDividerItemDecoration;
 import com.zl.dafeng.R;
 import com.zl.dafeng.bo.model.BelleModel;
 import com.zl.dafeng.dafeng.Constant;
@@ -22,6 +30,7 @@ import com.zl.dafeng.novate.Novate;
 import com.zl.dafeng.novate.Throwable;
 import com.zl.dafeng.ui.adapter.BelleAdapter;
 import com.zl.dafeng.ui.base.BaseFragment;
+import com.zl.dafeng.ui.widgetview.CustomLoadMoreView;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -31,6 +40,7 @@ import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import okhttp3.ResponseBody;
 
 
@@ -45,11 +55,23 @@ public class GirlFragment extends BaseFragment {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.belle_recycview)
-    SwipeMenuRecyclerView belleRecycview;
+    RecyclerView belleRecycview;
+    @BindView(R.id.swipe_refresh_layout)
+    SwipeRefreshLayout swipeRefreshLayout;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
 
     private BelleAdapter belleAdapter;
     private List<BelleModel.ShowapiResBodyBean.NewslistBean> NewslistBeanList = new ArrayList<BelleModel.ShowapiResBodyBean.NewslistBean>();
 
+    private static final int TOTAL_COUNTER = 18;
+
+    private static final int PAGE_SIZE = 15;
+
+    private int delayMillis = 1000;
+    private int mCurrentCounter = 0;
+    private boolean isErr;
+    private boolean mLoadMoreEndGone = false;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,19 +85,115 @@ public class GirlFragment extends BaseFragment {
     @Override
     protected void initData() {
         // 设置线性布局
-        belleRecycview.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
-        belleRecycview.smoothOpenLeftMenu(0);
-        belleRecycview.smoothOpenRightMenu(0);
-        belleRecycview.setLayoutManager(new GridLayoutManager(getActivity(),2));
+//        belleRecycview.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        // 设置网格布局
+        belleRecycview.setLayoutManager(new GridLayoutManager(getActivity(), 2));
 
+        /**
+         * RecyclerView-FlexibleDivider 添加分割线
+         *  .drawable(R.drawable.sample)
+         */
+        //添加分割线
+        belleRecycview.addItemDecoration(
+                new HorizontalDividerItemDecoration.Builder(getActivity())
+                        .color(Color.RED)
+                        .sizeResId(R.dimen.divider)
+                        .marginResId(R.dimen.leftmargin, R.dimen.rightmargin)
+                        .build());
+
+        belleRecycview.addItemDecoration(
+                new VerticalDividerItemDecoration.Builder(getActivity())
+                        .color(Color.RED)
+                        .size(getResources().getDimensionPixelSize(R.dimen.divider))
+                        .margin(getResources().getDimensionPixelSize(R.dimen.topmargin),
+                                getResources().getDimensionPixelSize(R.dimen.bottommargin))
+                        .build());
+        /**
+         * BaseRecyclerViewAdapterHelper 使用技巧
+         */
         belleAdapter = new BelleAdapter(getActivity(), NewslistBeanList);
+
+        // 1、设置显现效果动画（渐显、缩放、从下到上，从左到右、从右到左）也可以自定义
+//        belleAdapter.openLoadAnimation(BaseQuickAdapter.ALPHAIN);
+//        belleAdapter.openLoadAnimation(BaseQuickAdapter.SCALEIN);
+//        belleAdapter.openLoadAnimation(BaseQuickAdapter.SLIDEIN_BOTTOM);
+//        belleAdapter.openLoadAnimation(BaseQuickAdapter.SLIDEIN_LEFT);
+//        belleAdapter.openLoadAnimation(BaseQuickAdapter.SLIDEIN_RIGHT);
+        // 自定义动画如此轻松
+        belleAdapter.openLoadAnimation(new BaseAnimation() {
+            @Override
+            public Animator[] getAnimators(View view) {
+                return new Animator[]{
+                        ObjectAnimator.ofFloat(view, "scaleY", 1, 1.1f, 1),
+                        ObjectAnimator.ofFloat(view, "scaleX", 1, 1.1f, 1)
+                };
+            }
+        });
+        // 2、添加头部和尾部
+//        View headView = getActivity().getLayoutInflater().inflate(R.layout.view_head, (ViewGroup) belleRecycview.getParent(),false);
+//        View footView = getActivity().getLayoutInflater().inflate(R.layout.view_foot,(ViewGroup) belleRecycview.getParent() ,false);
+//        belleAdapter.addHeaderView(headView);
+//        belleAdapter.addFooterView(footView);
+        // 3、添加空布局
+//        View emptyView = getActivity().getLayoutInflater().inflate(R.layout.view_empty,(ViewGroup) belleRecycview.getParent() ,false);
+//        belleAdapter.setEmptyView(emptyView);
+        // 4、使用它加载更多
+        belleAdapter.setEnableLoadMore(true);
+        belleAdapter.setLoadMoreView(new CustomLoadMoreView());
+        mCurrentCounter = belleAdapter.getData().size();
+        belleAdapter.setOnLoadMoreListener(new BaseQuickAdapter.RequestLoadMoreListener() {
+            @Override
+            public void onLoadMoreRequested() {
+                swipeRefreshLayout.setEnabled(false);
+                belleRecycview.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // 获取的数量小于当前页面要展示的数量
+                        if (belleAdapter.getData().size() < PAGE_SIZE) {
+                            belleAdapter.loadMoreEnd(true);
+                        } else {
+                            if (mCurrentCounter >= TOTAL_COUNTER) {
+//                    pullToRefreshAdapter.loadMoreEnd();//default visible
+                                belleAdapter.loadMoreEnd(mLoadMoreEndGone);//true is gone,false is visible
+                            } else {
+                                if (isErr) {
+                                    mCurrentCounter = belleAdapter.getData().size();
+                                    belleAdapter.loadMoreComplete();
+                                } else {
+                                    isErr = true;
+                                    Toast.makeText(getActivity(), "网络错误", Toast.LENGTH_LONG).show();
+                                    belleAdapter.loadMoreFail();
+
+                                }
+                            }
+                            swipeRefreshLayout.setEnabled(true);
+                        }
+                    }
+
+                }, delayMillis);
+            }
+        });
         belleRecycview.setAdapter(belleAdapter);
+
         getBellePic();
     }
 
     @Override
     protected void initView() {
         toolBarTitle.setText(getString(R.string.girl_title));
+//        swipeRefreshLayout.setColorSchemeColors(getActivity().getResources().getColor(R.color.statusbar_color),
+//                getActivity().getResources().getColor(R.color.toolbar_color), getActivity().getResources().getColor(R.color.primary_color));
+//        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+//            @Override
+//            public void onRefresh() {
+//                swipeRefreshLayout.postDelayed(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        swipeRefreshLayout.setRefreshing(false);
+//                    }
+//                }, 3000);
+//            }
+//        });
     }
 
     @Override
@@ -133,5 +251,9 @@ public class GirlFragment extends BaseFragment {
             }
         });
 
+    }
+
+    @OnClick(R.id.fab)
+    public void onClick() {
     }
 }
